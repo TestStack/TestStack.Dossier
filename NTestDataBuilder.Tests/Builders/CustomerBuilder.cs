a8f5f451332c1d2dc7e88f1ee3b4a26022901542
Rob Moore <me@robdmoore.id.au>
2013-05-26 12:54:29 +0800

    Added test that checks using NBuilder works

---
+++ NTestDataBuilder.Tests/Builders/CustomerBuilder.cs
@@ -0,0 +1,41 @@
+﻿using NTestDataBuilder.Tests.Entities;
+
+namespace NTestDataBuilder.Tests.Builders
+{
+    class CustomerBuilder : DataBuilder<Customer, CustomerBuilder>
+    {
+        public CustomerBuilder()
+        {
+            WithFirstName("Rob");
+            WithLastName("Moore");
+            JoinedIn(2013);
+        }
+
+        public CustomerBuilder WithFirstName(string firstName)
+        {
+            Set(x => x.FirstName, firstName);
+            return this;
+        }
+
+        public CustomerBuilder WithLastName(string lastName)
+        {
+            Set(x => x.LastName, lastName);
+            return this;
+        }
+
+        public CustomerBuilder JoinedIn(int yearJoined)
+        {
+            Set(x => x.YearJoined, yearJoined);
+            return this;
+        }
+
+        public override Customer Build()
+        {
+            return new Customer(
+                Get(x => x.FirstName),
+                Get(x => x.LastName),
+                Get(x => x.YearJoined)
+            );
+        }
+    }
+}
--- NTestDataBuilder.Tests/CreateListTests.cs
+++ NTestDataBuilder.Tests/CreateListTests.cs
@@ -1,4 +1,6 @@
-﻿using NTestDataBuilder.Tests.Builders;
+﻿using System.Linq;
+using FizzWare.NBuilder;
+using NTestDataBuilder.Tests.Builders;
 using NUnit.Framework;
 
 namespace NTestDataBuilder.Tests
@@ -26,13 +28,25 @@ namespace NTestDataBuilder.Tests
         }
 
         [Test]
-        public void GivenCreatingListOfSizeX_WhenListIsBuilt_ThenAListOfTheBuildersIsGenerated()
+        public void GivenCreatingListOfSizeX_WhenListIsBuilt_ThenAListOfTheRightBuilderTypesIsGenerated()
         {
             var builderExpression = BasicCustomerBuilder.CreateListOfSize(5);
 
             var builtList = builderExpression.Build();
 
             Assert.That(builtList, Has.All.With.TypeOf<BasicCustomerBuilder>());
+        }
+
+        [Test]
+        public void GivenCreatingAList_WhenUsingNBuilderToChangeTheList_ThenTheChangesAreReflected()
+        {
+            var generator = new SequentialGenerator<int>();
+            
+            var list = CustomerBuilder.CreateListOfSize(3)
+                .All().With(b => b.WithFirstName(generator.Generate().ToString()))
+                .Build();
+
+            Assert.That(list.Select(b => b.Get(x => x.FirstName)), Is.EqualTo(new[]{"0", "1", "2"}));
         }
     }
 }
