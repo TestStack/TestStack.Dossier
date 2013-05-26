ab3879575714c2feb6993ce7fb832590d956bdde
Rob Moore <me@robdmoore.id.au>
2013-05-26 10:43:55 +0800

    Added basic get/set functionality

---
+++ NTestDataBuilder.Tests/GetSetTests.cs
@@ -0,0 +1,59 @@
+﻿using NTestDataBuilder.Tests.Builders;
+using NUnit.Framework;
+
+namespace NTestDataBuilder.Tests
+{
+    class GetSetTests
+    {
+        [Test]
+        public void GivenAValueHasBeenSetAgainstAProperty_WhenRetrievingTheValueForThatProperty_ThenTheSetValueIsReturned()
+        {
+            const string set = "Value";
+            var b = new BasicCustomerBuilder();
+            b.Set(x => x.FirstName, set);
+
+            var retrieved = b.Get(x => x.FirstName);
+
+            Assert.That(retrieved, Is.EqualTo(set));
+        }
+
+        [Test]
+        public void GivenTwoValuesHaveBeenSetAgainstAProperty_WhenRetrievingTheValueForThatProperty_ThenTheLastSetValueIsReturned()
+        {
+            const string set = "Value";
+            var b = new BasicCustomerBuilder();
+            b.Set(x => x.FirstName, "random");
+            b.Set(x => x.FirstName, set);
+
+            var retrieved = b.Get(x => x.FirstName);
+
+            Assert.That(retrieved, Is.EqualTo(set));
+        }
+
+        [Test]
+        public void GivenAValueHasBeenSetAgainstTwoProperties_WhenRetrievingTheValueForTheFirstSetProperty_ThenTheSetValueIsReturned()
+        {
+            const string set = "Value";
+            var b = new BasicCustomerBuilder();
+            b.Set(x => x.FirstName, set);
+            b.Set(x => x.LastName, "random");
+
+            var retrieved = b.Get(x => x.FirstName);
+
+            Assert.That(retrieved, Is.EqualTo(set));
+        }
+
+        [Test]
+        public void GivenAValueHasBeenSetAgainstTwoProperties_WhenRetrievingTheValueForTheSecondSetProperty_ThenTheSetValueIsReturned()
+        {
+            const string set = "Value";
+            var b = new BasicCustomerBuilder();
+            b.Set(x => x.FirstName, "random");
+            b.Set(x => x.LastName, set);
+
+            var retrieved = b.Get(x => x.LastName);
+
+            Assert.That(retrieved, Is.EqualTo(set));
+        }
+    }
+}
--- NTestDataBuilder/DataBuilder.cs
+++ NTestDataBuilder/DataBuilder.cs
@@ -1,4 +1,8 @@
-﻿namespace NTestDataBuilder
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace NTestDataBuilder
 {
     /// <summary>
     /// Generates objects of type <see cref="T"/>.
@@ -22,10 +26,43 @@
         where TEntity : class
         where TBuilder : class, IDataBuilder<TEntity>
     {
+        private readonly Dictionary<string, object> _properties = new Dictionary<string, object>();
+
         /// <summary>
         /// Build the object.
         /// </summary>
         /// <returns>The built object</returns>
         public abstract TEntity Build();
+
+        /// <summary>
+        /// Records the given value for the given property from <see cref="TEntity"/>.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the property</typeparam>
+        /// <param name="property">A lambda expression specifying the property to record a value for</param>
+        /// <param name="value">The value to record</param>
+        public void Set<TValue>(Expression<Func<TEntity, TValue>> property, TValue value)
+        {
+            _properties[GetPropertyName(property)] = value;
+        }
+
+        /// <summary>
+        /// Gets the recorded value for the given property from <see cref="TEntity"/>.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the property</typeparam>
+        /// <param name="property">A lambda expression specifying the property to retrieve the recorded value for</param>
+        /// <returns>The recorded value of the property</returns>
+        public TValue Get<TValue>(Expression<Func<TEntity, TValue>> property)
+        {
+            return (TValue)_properties[GetPropertyName(property)];
+        }
+
+        private static string GetPropertyName<TValue>(Expression<Func<TEntity, TValue>> property)
+        {
+            var memExp = property.Body as MemberExpression;
+            if (memExp == null)
+                throw new ArgumentException("Property must be valid on the entity object", "property");
+
+            return memExp.Member.Name;
+        }
     }
 }
