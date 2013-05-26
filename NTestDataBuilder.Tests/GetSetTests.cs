c9c96632329b57d936afd338698c58f100ebe49f
Rob Moore <me@robdmoore.id.au>
2013-05-26 10:45:40 +0800

    Refactored tests

--- NTestDataBuilder.Tests/GetSetTests.cs
+++ NTestDataBuilder.Tests/GetSetTests.cs
@@ -5,55 +5,56 @@ namespace NTestDataBuilder.Tests
 {
     class GetSetTests
     {
+        private BasicCustomerBuilder _b;
+        const string SetValue = "Value";
+
+        [SetUp]
+        public void Setup()
+        {
+            _b = new BasicCustomerBuilder();
+        }
+
         [Test]
         public void GivenAValueHasBeenSetAgainstAProperty_WhenRetrievingTheValueForThatProperty_ThenTheSetValueIsReturned()
         {
-            const string set = "Value";
-            var b = new BasicCustomerBuilder();
-            b.Set(x => x.FirstName, set);
+            _b.Set(x => x.FirstName, SetValue);
 
-            var retrieved = b.Get(x => x.FirstName);
+            var retrieved = _b.Get(x => x.FirstName);
 
-            Assert.That(retrieved, Is.EqualTo(set));
+            Assert.That(retrieved, Is.EqualTo(SetValue));
         }
 
         [Test]
         public void GivenTwoValuesHaveBeenSetAgainstAProperty_WhenRetrievingTheValueForThatProperty_ThenTheLastSetValueIsReturned()
         {
-            const string set = "Value";
-            var b = new BasicCustomerBuilder();
-            b.Set(x => x.FirstName, "random");
-            b.Set(x => x.FirstName, set);
+            _b.Set(x => x.FirstName, "random");
+            _b.Set(x => x.FirstName, SetValue);
 
-            var retrieved = b.Get(x => x.FirstName);
+            var retrieved = _b.Get(x => x.FirstName);
 
-            Assert.That(retrieved, Is.EqualTo(set));
+            Assert.That(retrieved, Is.EqualTo(SetValue));
         }
 
         [Test]
         public void GivenAValueHasBeenSetAgainstTwoProperties_WhenRetrievingTheValueForTheFirstSetProperty_ThenTheSetValueIsReturned()
         {
-            const string set = "Value";
-            var b = new BasicCustomerBuilder();
-            b.Set(x => x.FirstName, set);
-            b.Set(x => x.LastName, "random");
+            _b.Set(x => x.FirstName, SetValue);
+            _b.Set(x => x.LastName, "random");
 
-            var retrieved = b.Get(x => x.FirstName);
+            var retrieved = _b.Get(x => x.FirstName);
 
-            Assert.That(retrieved, Is.EqualTo(set));
+            Assert.That(retrieved, Is.EqualTo(SetValue));
         }
 
         [Test]
         public void GivenAValueHasBeenSetAgainstTwoProperties_WhenRetrievingTheValueForTheSecondSetProperty_ThenTheSetValueIsReturned()
         {
-            const string set = "Value";
-            var b = new BasicCustomerBuilder();
-            b.Set(x => x.FirstName, "random");
-            b.Set(x => x.LastName, set);
+            _b.Set(x => x.FirstName, "random");
+            _b.Set(x => x.LastName, SetValue);
 
-            var retrieved = b.Get(x => x.LastName);
+            var retrieved = _b.Get(x => x.LastName);
 
-            Assert.That(retrieved, Is.EqualTo(set));
+            Assert.That(retrieved, Is.EqualTo(SetValue));
         }
     }
 }
