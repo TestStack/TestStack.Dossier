606460e173cc877d6d7f032917da2990eaed0829
Rob Moore <me@robdmoore.id.au>
2013-05-26 14:00:52 +0800

    Added xmldoc comments

--- NTestDataBuilder/DataBuilderExtensions.cs
+++ NTestDataBuilder/DataBuilderExtensions.cs
@@ -13,9 +13,9 @@ namespace NTestDataBuilder
         /// Builds a list of entities given an NBuilder list expression of data builders.
         /// </summary>
         /// <typeparam name="TBuilder">The type of the builder being built using NBuilder</typeparam>
-        /// <typeparam name="TEntity"></typeparam>
-        /// <param name="builderList"></param>
-        /// <returns></returns>
+        /// <typeparam name="TEntity">The type of object being generated</typeparam>
+        /// <param name="builderList">The NBuilder list of builders</param>
+        /// <returns>The built list of objects</returns>
         public static IList<TEntity> BuildDataList<TEntity, TBuilder>(this IOperable<TBuilder> builderList)
             where TBuilder : IDataBuilder<TEntity>
             where TEntity : class
@@ -24,12 +24,12 @@ namespace NTestDataBuilder
         }
 
         /// <summary>
-        /// 
+        /// Builds a list of entities given an NBuilder list expression of data builders.
         /// </summary>
-        /// <typeparam name="TBuilder"></typeparam>
-        /// <typeparam name="TEntity"></typeparam>
-        /// <param name="builderList"></param>
-        /// <returns></returns>
+        /// <typeparam name="TBuilder">The type of the builder being built using NBuilder</typeparam>
+        /// <typeparam name="TEntity">The type of object being generated</typeparam>
+        /// <param name="builderList">The NBuilder list of builders</param>
+        /// <returns>The built list of objects</returns>
         public static IList<TEntity> BuildDataList<TEntity, TBuilder>(this IListBuilder<TBuilder> builderList)
             where TBuilder : IDataBuilder<TEntity>
             where TEntity : class
