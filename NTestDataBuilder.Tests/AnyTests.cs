40dfd3207d0ce3b99bbb7ef094132b29dd1a842d
Rob Moore <me@robdmoore.id.au>
2014-09-07 01:26:39 +0800

    Added initial extension method to generate localised value for equivalence class (Any.String())

---
+++ NTestDataBuilder.Tests/AnyTests.cs
@@ -0,0 +1,26 @@
+﻿using NTestDataBuilder.EquivalenceClasses;
+using NUnit.Framework;
+
+namespace NTestDataBuilder.Tests
+{
+    public class AnyTests
+    {
+        public AnonymousValueFixture Any { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Any = new AnonymousValueFixture();
+        }
+
+        [Test]
+        public void WhenGettingAnyString_ThenDifferentGuidStringsEveryTime()
+        {
+            var s1 = Any.String();
+            var s2 = Any.String();
+
+            Assert.That(s1, Is.Not.EqualTo(s2), "Generated strings aren't different");
+            Assert.That(new[] { s1, s2 }, Has.All.StringMatching("^[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}$"));
+        }
+    }
+}
---
+++ NTestDataBuilder/EquivalenceClasses/StringEquivalenceClasses.cs
@@ -0,0 +1,21 @@
+﻿using NTestDataBuilder.Suppliers;
+
+namespace NTestDataBuilder.EquivalenceClasses
+{
+    /// <summary>
+    /// Extension methods that describe equivalence classes for generating anonymous string values.
+    /// </summary>
+    public static class StringEquivalenceClasses
+    {
+        /// <summary>
+        /// Generate and return a string.
+        /// </summary>
+        /// <param name="fixture">The fixture to generate a string for</param>
+        /// <returns>The generated string</returns>
+        public static string String(this AnonymousValueFixture fixture)
+        {
+            return new StringValueSupplier()
+                .GenerateAnonymousValue<object, string>(fixture, "");
+        }
+    }
+}
