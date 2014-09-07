49db41f65a3d6fe6ab7573646c73c35c11001827
Rob Moore <me@robdmoore.id.au>
2014-09-07 12:59:13 +0800

    Removed compiler warnings

--- NTestDataBuilder/TestDataBuilder.cs
+++ NTestDataBuilder/TestDataBuilder.cs
@@ -6,7 +6,7 @@ using FizzWare.NBuilder;
 namespace NTestDataBuilder
 {
     /// <summary>
-    /// Generates objects of type <see cref="T"/>.
+    /// Generates objects of type {T}.
     /// </summary>
     /// <typeparam name="T">The type of object this class generates</typeparam>
     public interface ITestDataBuilder<out T> where T : class
@@ -19,7 +19,7 @@ namespace NTestDataBuilder
     }
 
     /// <summary>
-    /// Base class definining infrastructure for a class that generates objects of type <see cref="TObject"/>.
+    /// Base class definining infrastructure for a class that generates objects of type {TObject}.
     /// </summary>
     /// <typeparam name="TObject">The type of object this class generates</typeparam>
     /// <typeparam name="TBuilder">The type for this class, yes this is a recursive type definition</typeparam>
@@ -83,7 +83,7 @@ namespace NTestDataBuilder
         protected virtual void AlterProxy(TObject proxy) {}
         
         /// <summary>
-        /// Records the given value for the given property from <see cref="TObject"/>.
+        /// Records the given value for the given property from {TObject}.
         /// </summary>
         /// <typeparam name="TValue">The type of the property</typeparam>
         /// <param name="property">A lambda expression specifying the property to record a value for</param>
@@ -95,7 +95,7 @@ namespace NTestDataBuilder
         }
 
         /// <summary>
-        /// Gets the recorded value for the given property from <see cref="TObject"/>.
+        /// Gets the recorded value for the given property from {TObject}.
         /// </summary>
         /// <typeparam name="TValue">The type of the property</typeparam>
         /// <param name="property">A lambda expression specifying the property to retrieve the recorded value for</param>
@@ -109,12 +109,12 @@ namespace NTestDataBuilder
         }
 
         /// <summary>
-        /// Gets the recorded value for the given property from <see cref="TObject"/> or if no
-        /// value has been recorded the default value for <see cref="TValue"/>.
+        /// Gets the recorded value for the given property from {TObject} or if no
+        /// value has been recorded the default value for {TValue}.
         /// </summary>
         /// <typeparam name="TValue">The type of the property</typeparam>
         /// <param name="property">A lambda expression specifying the property to retrieve the recorded value for</param>
-        /// <returns>The recorded value of the property or teh default value for <see cref="TValue"/> if no value recorded</returns>
+        /// <returns>The recorded value of the property or teh default value for {TValue} if no value recorded</returns>
         public TValue GetOrDefault<TValue>(Expression<Func<TObject, TValue>> property)
         {
             return Has(property)
@@ -127,14 +127,14 @@ namespace NTestDataBuilder
         /// When you are done call .Build().Select(b => b.Build()) to get the list of entities.
         /// </summary>
         /// <param name="size">The size of list</param>
-        /// <returns>The NBuilder list builder for a list of <see cref="TBuilder"/> of the specified size</returns>
+        /// <returns>The NBuilder list builder for a list of {TBuilder} of the specified size</returns>
         public static IListBuilder<TBuilder> CreateListOfSize(int size)
         {
             return Builder<TBuilder>.CreateListOfSize(size);
         }
 
         /// <summary>
-        /// Returns whether or not there is currently an explicit value recorded against the given property from <see cref="TObject"/>.
+        /// Returns whether or not there is currently an explicit value recorded against the given property from {TObject}.
         /// </summary>
         /// <typeparam name="TValue">The type of the property</typeparam>
         /// <param name="property">A lambda expression specifying the property to retrieve the recorded value for</param>
