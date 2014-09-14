8a657999093cdfb43ae61939d285cd8f8e754c82
Rob Moore <me@robdmoore.id.au>
2014-09-14 21:14:03 +0800

    Moving test around and added tests for string equivalence classes

---
+++ NTestDataBuilder.Tests/EquivalenceClasses/StringEquivalenceClassesTests.cs
@@ -0,0 +1,45 @@
+﻿using NTestDataBuilder.EquivalenceClasses;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NTestDataBuilder.Tests.EquivalenceClasses
+{
+    public class StringEquivalenceClassesTests
+    {
+        public AnonymousValueFixture Any { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Any = new AnonymousValueFixture();
+        }
+
+        [Test]
+        public void WhenGettingAnyString_ThenReturnDifferentNonEmptyStringsEveryTime()
+        {
+            var s1 = Any.String();
+            var s2 = Any.String();
+
+            s1.ShouldBeOfType<string>();
+            s2.ShouldBeOfType<string>();
+            s1.ShouldNotBeNullOrEmpty();
+            s2.ShouldNotBeNullOrEmpty();
+            s1.ShouldNotBe(s2);
+        }
+
+        [Test]
+        public void WhenGettingAnyStringStartingWithSomething_ThenReturnDifferentStringsStartingWithThatStringEveryTime()
+        {
+            var s1 = Any.StringStartingWith("St4rt");
+            var s2 = Any.StringStartingWith("St4rt");
+            var s3 = Any.StringStartingWith("Something Else");
+
+            s1.ShouldNotBe(s2);
+            s1.ShouldNotBe(s3);
+            s2.ShouldNotBe(s3);
+            s1.ShouldStartWith("St4rt");
+            s2.ShouldStartWith("St4rt");
+            s3.ShouldStartWith("Something Else");
+        }
+    }
+}
--- NTestDataBuilder/EquivalenceClasses/StringEquivalenceClasses.cs
+++ NTestDataBuilder/EquivalenceClasses/StringEquivalenceClasses.cs
@@ -27,5 +27,16 @@ namespace NTestDataBuilder.EquivalenceClasses
         {
             return fixture.Fixture.Create(prefix);
         }
+
+        /// <summary>
+        /// Generate and return a string ending with the given suffix.
+        /// </summary>
+        /// <param name="fixture">The fixture to generate a string for</param>
+        /// <param name="suffix">String to end the returned anonymous string with</param>
+        /// <returns>The generated string</returns>
+        public static string StringEndingWith(this AnonymousValueFixture fixture, string suffix)
+        {
+            return string.Format("{0}{1}", fixture.Fixture.Create<string>(), suffix);
+        }
     }
 }
