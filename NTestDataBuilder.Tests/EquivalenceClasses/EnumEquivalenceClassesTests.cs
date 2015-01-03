428ecff2b4fc7dccdcb7c3f6722578e3f788d9e8
Rob Moore <me@robdmoore.id.au>
2015-01-03 22:55:53 +0800

    Added enum equivalence classes

---
+++ NTestDataBuilder.Tests/EquivalenceClasses/EnumEquivalenceClassesTests.cs
@@ -0,0 +1,53 @@
+﻿using NTestDataBuilder.EquivalenceClasses;
+using Shouldly;
+using Xunit;
+
+namespace NTestDataBuilder.Tests.EquivalenceClasses
+{
+    public class EnumEquivalenceClassesTests
+    {
+        public AnonymousValueFixture Any { get; private set; }
+
+        public EnumEquivalenceClassesTests()
+        {
+            Any = new AnonymousValueFixture();
+        }
+
+        [Fact]
+        public void WhenGettingAnyOfEnum_ThenReturnSequentialtEnumValueEveryTimeUntilItWrapsAroundAgain()
+        {
+            var values = new[]
+            {
+                Any.Of<TestEnum>(),
+                Any.Of<TestEnum>(),
+                Any.Of<TestEnum>(),
+                Any.Of<TestEnum>(),
+                Any.Of<TestEnum>()
+            };
+
+            values.ShouldBe(new []{TestEnum.One, TestEnum.Two, TestEnum.Three, TestEnum.Four, TestEnum.One});
+        }
+
+        [Fact]
+        public void WhenGettingAnyEnumExceptOneValue_ThenReturnSequentialEnumValueExceptTheExceptionEveryTimeUntilItWrapsAroundAgain()
+        {
+            var values = new[]
+            {
+                Any.Except(TestEnum.Two),
+                Any.Except(TestEnum.Two),
+                Any.Except(TestEnum.Two),
+                Any.Except(TestEnum.Two)
+            };
+
+            values.ShouldBe(new[] { TestEnum.One, TestEnum.Three, TestEnum.Four, TestEnum.One });
+        }
+    }
+
+    enum TestEnum
+    {
+        One,
+        Two,
+        Three,
+        Four
+    }
+}
