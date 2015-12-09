db8845e4637646a90e706693d383285f726dc2ab
Dennis Roche <droche@abngroup.com.au>
2015-12-09 09:49:45 +0800

    Added ITestDataBuilder interface

---
+++ TestStack.Dossier/ITestDataBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace TestStack.Dossier
+{
+    /// <summary>
+    /// Base class definining infrastructure for a class that generates objects of type {TObject}.
+    /// </summary>
+    /// <typeparam name="TObject">The type of object this class generates</typeparam>
+    public interface ITestDataBuilder<out TObject> where TObject : class
+    {
+        /// <summary>
+        /// Build the object.
+        /// </summary>
+        /// <returns>The built object</returns>
+        TObject Build();
+    }
+}
