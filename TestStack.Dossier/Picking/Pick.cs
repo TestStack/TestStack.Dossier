9eea6a147882512ea487910261d168b82e2f0861
Michael Whelan <mjmwdev@gmail.com>
2016-01-19 21:26:39 +0000

    Added Pick functionality to builders

---
+++ TestStack.Dossier/Picking/Pick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace TestStack.Dossier.Picking
+{
+    /// <summary>
+    /// Pick a sequence of items from a collection of items according to different selection strategies.
+    /// </summary>
+    public class Pick
+    {
+        /// <summary>
+        /// Selects a random item from the list each time it is called.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">The list.</param>
+        /// <returns>The RandomItemPicker class.</returns>
+        public static RandomItemPicker<T> RandomItemFrom<T>(IList<T> list)
+        {
+            return new RandomItemPicker<T>(list);
+        }
+
+        /// <summary>
+        /// Selects each item sequentially from the list and starts again from the beginning when the list is exhausted.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">The list.</param>
+        /// <returns></returns>
+        public static RepeatingSequenceItemPicker<T> RepeatingSequenceFrom<T>(IList<T> list)
+        {
+            return new RepeatingSequenceItemPicker<T>(list);
+        }
+    }
+}
---
+++ TestStack.Dossier/Picking/RandomItemPicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TestStack.Dossier.DataSources.Generators;
+
+namespace TestStack.Dossier.Picking
+{
+    /// <summary>
+    /// Implements the random item strategy
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="ItemPicker{T}" />
+    public class RandomItemPicker<T> : ItemPicker<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomItemPicker{T}"/> class.
+        /// </summary>
+        /// <param name="list">The list.</param>
+        public RandomItemPicker(IList<T> list)
+            : base(list, new RandomGenerator()) { }
+    }
+}
