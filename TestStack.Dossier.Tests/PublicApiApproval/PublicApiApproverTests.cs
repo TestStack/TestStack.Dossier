da79c07e2f9290a9b218c158aff94da5d25837cd
Michael Whelan <mjmwdev@gmail.com>
2016-02-11 12:46:20 +0000

    Added Public API test to prevent accidental API changes.

---
+++ TestStack.Dossier.Tests/PublicApiApproval/PublicApiApproverTests.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace TestStack.Dossier.Tests.PublicApiApproval
+{
+    public class PublicApiApproverTests
+    {
+        [Fact]
+        public void GivenDossierAssembly_WhenPublicApiChecked_ShouldHaveNoChanges()
+        {
+            ShouldlyConfiguration.DiffTools.KnownDoNotLaunchStrategies.TeamCity.ShouldNotLaunch();
+            var dossierAssembly = typeof (AnonymousValueFixture).Assembly;
+            var publicApi = PublicApiGenerator.PublicApiGenerator.GetPublicApi(dossierAssembly);
+            publicApi.ShouldMatchApproved();
+        }
+    }
+}
